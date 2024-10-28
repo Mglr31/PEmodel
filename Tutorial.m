6ec85f4d1ad52a45229062dced2dd47d3afc01f3
Martin Gillier <140694793+Mglr31@users.noreply.github.com>
2024-10-28 15:26:35 +0100

    Add files via upload

--- Tutorial.m
+++ Tutorial.m
@@ -1,10 +1,11 @@
-%simulation grid with atmospheric data representing the Martian atmosphere
+%Simulation grid with atmospheric data representing the Martian atmosphere
 %at 12 local time at Perseverance site
 load("simu12.mat")
-simu12.Zabs=20;%Max alt with realistic results
-simu12.Zmax=40;%Max alt of the grid
+simu12.Zabs=20;%Max alttitude of the sound field
+simu12.Zmax=40;%Max alttitude of the grid
 simu12.WSpeed=1;%adding wind to the simulation
-%84 Hz, 80 dB source at 5 m above the ground 
+simu12.ZGround=10+10i;%Ground acoustic impedance
+%loading the source
 load("source.mat")
 source.Amplitude=100;%changing amplitude to 100 dB
 source.freq=100;
@@ -17,6 +18,5 @@ plot_PE(pe,["spl"])
 %% Displaying a slice of SPL at 3 m
 plot_slice(pe,3)
 
-%% Separate the different effects responsible for the losses
-effects=separateEffectsPE(simu12,source,1.5,'Turbulence',0);%no effect of turbulence
-plot_effectsPE(effects,1.5,"allnosum");
+
+
